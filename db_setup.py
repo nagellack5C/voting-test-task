414be0ba84df6e00c882a57c809bd21c538d2c23
Aleksandr Klimov <aoklimov501@gmail.com>
2019-11-23 16:56:19 +0300

    mongodb config connection added

--- db_setup.py
+++ db_setup.py
@@ -1,22 +1,26 @@
 import pymongo
 
 
+MONGO_URL = "ds057568.mlab.com"
+MONGO_PORT = 57568
+USERNAME = 'testtest'
+PASSWORD = '1amtesting'
+
+
 def start():
-    client = pymongo.MongoClient("127.0.0.1", 27017)
-    mydb = client["voting-service"]
-    mydb.command("dropDatabase")
+    client = pymongo.MongoClient(f'mongodb://{USERNAME}:{PASSWORD}@ds057568.mlab.com:57568/voting-service?retryWrites=false')
     mydb = client["voting-service"]
     mydb.create_collection("votings")
-    # mydb["votings"].create_index([('voting_id', pymongo.ASCENDING)])
+    mydb["votings"].create_index([('voting_id', pymongo.ASCENDING)])
     mydb.create_collection("users")
     mydb["users"].create_index([('username', pymongo.ASCENDING)], unique=True)
     mydb["users"].insert_one({
         'username': 'admin',
         'b64creds': 'YWRtaW46YWRtaW4='
     })
-    print(client.list_database_names())
     assert(not list(mydb["votings"].find({})))
     assert len(list(mydb["users"].find({}))) == 1
+
 
 if __name__ == "__main__":
     start()
